16b78e57742314ebaac02069387c36d31c4de050
ryanpiegols <101875640+ryanpiegols@users.noreply.github.com>
2022-04-01 08:53:27 -0400

    up to date make/model queries
    this is what i currently have on the make/model queries. As discussed, im not able to effectively test them on pycharm with flask and combining with the main.py using callDatabase function. Also, the inj_person_count is still unable to be found.

--- MakeModel.py
+++ MakeModel.py
@@ -6,32 +6,27 @@ global cursor
 connection = sql.connect('aircraftDB')
 cursor = connection.cursor()
 
-class Aircraft():
-    def __init__(self, acft_make, acft_model):
-        self.acft_make = acft_make
-        self.acft_model  = acft_model
-        
-    def getAirList(self):
-        return [self.acft_make, self.acft_model]
+#make = 'PIPER'
+#model = 'PA 28-180'
+
+def callDatabase(make, model):
+    displayMakeModel(make, model)
+
+def displayMakeModel(make, model):
+    input = (make, model)
     
-def callDatabase():
-    displayMake()
-    # displayMakeModel()
-    
-def displayMake():
-    cursor.execute("""SELECT * FROM aircraft WHERE acft_make = ?;""")
-    result = cursor.fetchall()
-    
+    cursor.execute("""SELECT eng_type FROM engines,aircraft
+                    WHERE acft_model = ? AND acft_make = ?;""", input)
+    cursor.execute("""SELECT inj_person_count FROM injury,aircraft
+                    WHERE acft_model = ? AND acft_make = ? AND injury_level = 'FATL';""", input)
+    cursor.execute("""SELECT ev_state FROM events,aircraft
+                    WHERE acft_model = ? AND acft_make = ?;""", input)
+    cursor.execute("""SELECT ev_date FROM events,aircraft
+                    WHERE acft_model = ? AND acft_make = ?;""", input)
+    cursor.execute("""SELECT eng_mfgr FROM engines,aircraft
+                    WHERE acft_model = ? AND acft_make = ?;""", input)
+
     for names in result:
-        print("\n    ", names[0])
-        
-# def displayMakeModel():
-    
-#     cursor.execute("""SELECT * FROM aircraft WHERE acft_model = ?;""")
-#     result = cursor.fetchall()
-    
-#     for names in result:
-#         print("\n    ", names[0])
-        
-callDatabase()
+        print("\n", names[0])
+
 connection.close()
